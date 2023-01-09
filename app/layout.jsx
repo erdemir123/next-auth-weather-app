51b7314901e3c70d7c65c13220065abbd36cc05a
erdemir123 <sadikerdemir93@gmail.com>
2023-01-10 00:45:20 +0300

    neaxt-auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,24 @@
+import Image from 'next/image'
+import login from "../images/login.jpg"
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+     
       <head />
-      <body>{children}</body>
+      <body>
+        <div className='flex h-screen bg-blue-400 shadow-md '>
+          <div className='m-auto bg-slate-50 rounded-lg w-4/5 h-3/4 lg:flex shadow-2xl'>
+            <div className='h-[50%] lg:w-[50%] lg:h-[100%]'>
+              <Image src={login} className="w-[100%] h-[100%] rounded-lg" alt='login-image' priority/>
+            </div>
+              <div className='h-[50%] lg:w-[50%] lg:h-[100%] '>
+              {children}
+              </div>
+          </div>
+        </div>
+        </body>
     </html>
   )
 }
---
+++ components/Login.jsx
@@ -0,0 +1,182 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [register, setRegister] = useState(true);
+  const handlelogin = (e) => {
+    e.preventDefault();
+    setRegister(!register);
+    console.log(register);
+  };
+  const [registerUser, setRegisterUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const { username, email, password } = registerUser;
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if (username && email && password) {
+      setRegister(!register);
+      setRegisterUser("");
+      console.log(registerUser)
+    } else {
+      alert("!");
+    }
+  };
+  return register ? (
+    <div className="w-[100%] mx-4 mt-14">
+      <div>
+        <form>
+          <div className="flex flex-row items-center justify-center lg:justify-start">
+            <p className="text-lg mb-0 mr-4">Sign in with</p>
+            <button
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+            >
+              {/* Facebook */}
+              <svg width="20" height="20" viewBox="0 0 256 256"><path fill="currentColor" d="M128 228a100 100 0 1 1 70.7-170.7a12 12 0 0 1 0 17a12.2 12.2 0 0 1-17 0A75.2 75.2 0 0 0 128 52a76 76 0 1 0 75.1 88H128a12 12 0 0 1 0-24h88a12 12 0 0 1 12 12a100.2 100.2 0 0 1-100 100Z"/></svg>
+            </button>
+            <button
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+            >
+              {/* Twitter */}
+              <svg width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"/></svg>
+            </button>
+            
+          </div>
+          <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+            <p className="text-center font-semibold mx-4 mb-0">Or</p>
+          </div>
+          {/* Email input */}
+          <div className="mb-2">
+            <input
+              type="text"
+              className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Email address"
+            />
+          </div>
+          {/* Password input */}
+          <div className="mb-2">
+            <input
+              type="password"
+              className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex justify-between items-center ">
+            <div className="form-group form-check">
+              <input
+                type="checkbox"
+                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                id="exampleCheck2"
+              />
+              <label
+                className="form-check-label inline-block text-gray-800"
+                htmlFor="exampleCheck2"
+              >
+                Remember me
+              </label>
+            </div>
+            <Link href="#!" className="text-gray-800">
+              Forgot password?
+            </Link>
+          </div>
+          <div className="text-center lg:text-left">
+            <button
+              type="button"
+              className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mb-8"
+            >
+              Login
+            </button>
+            <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+              Don't have an account?
+              <button
+                className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                onClick={handlelogin}
+              >
+                Register
+              </button>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  ) : (
+    <div className="w-[100%] mx-4 mt-1 ">
+      <section>
+        <div className="px-6 h-full text-gray-800 w-[100%] mx-4 mt-1 ">
+          <div className=" w-[100%] flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+            <div className="w-[100%] ">
+              {/* Name input */}
+              <div className="mb-6 w-[100%]">
+                <input
+                  type="text"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlInput1"
+                  placeholder="UserName"
+                  required
+                  value={registerUser?.username}
+                  onChange={(e) =>
+                    setRegisterUser({
+                      ...registerUser,
+                      username: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              {/* Email input */}
+              <div className="mb-6">
+                <input
+                  type="email"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlInput2"
+                  placeholder="Email address"
+                  required
+                  value={registerUser?.email}
+                  onChange={(e) =>
+                    setRegisterUser({ ...registerUser, email: e.target.value })
+                  }
+                />
+              </div>
+              {/* Password input */}
+              <div className="mb-6">
+                <input
+                  type="password"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlInput3"
+                  placeholder="Password"
+                  required
+                  value={registerUser?.password}
+                  onChange={(e) =>
+                    setRegisterUser({
+                      ...registerUser,
+                      password: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <div className="flex justify-between text-center flex-col lg:flex-row gap-4 items-center lg:justify-center">
+                <button
+                  type="button"
+                  className="w-32 px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mb-8"
+                  onClick={handleRegister}
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;

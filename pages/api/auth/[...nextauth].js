14f35fe6116c3c2b8e9ef7278267ff777839aa57
erdemir123 <sadikerdemir93@gmail.com>
2023-01-13 03:22:47 +0300

    process.env added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,12 +8,12 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
       GoogleProvider({
-        clientId: "530717775078-ivsp90bru0242pqp3fpadsdinscnur0j.apps.googleusercontent.com",
-            clientSecret: "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"
+        clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
       }),
       GitHubProvider({
-        clientId: "ced3cf60994a01ea5dd5",
-        clientSecret: "a7bd9baa6d0c4f32e63fa9f3ead47f8f266e379b"
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
       })
       // ...add more providers here
     ],
